fixed quality calculation speed

MotifCompendium/utils/clustering.py
@@ -13,7 +13,7 @@ def cluster(
     algorithm: str,
     similarity_threshold: float = 0.9,
     **kwargs,
-    ) -> list[int]:
+) -> list[int]:
     """Cluster a similarity matrix.
 
     Given a similarity matrix, a similarity threshold, and an algorithm choice, return
@@ -47,36 +47,41 @@ def cluster(
     elif algorithm == "greedy_cc":
         adjacency_matrix = similarity_matrix >= similarity_threshold
         return greedy_cc_clustering(adjacency_matrix)
-    
+
     # Spectral
     elif algorithm == "spectral":
         kwargs_filtered = {
             k: v for k, v in enumerate(kwargs) if k in ["k", "cluster_qr"]
         }
         return spectral_clustering_k(similarity_matrix, **kwargs_filtered)
-    
+
     else:
         raise ValueError(f"Unsupported clustering algorithm: {algorithm}")
+
 
 #####################
 # LEIDEN CLUSTERING #
 #####################
 
-def create_sparse_csr(similarity_matrix, similarity_threshold=0.5):
-	"""Create a sparse version of the similarity matrix, 
-      by filtering out scores below a similarity_threshold"""
-	sparse_similarity_matrix = sparse.csr_matrix(np.where(similarity_matrix >= similarity_threshold, similarity_matrix, 0))
-	return sparse_similarity_matrix
 
-def run_leiden_clustering(similarity_matrix: np.ndarray, 
+def create_sparse_csr(similarity_matrix, similarity_threshold=0.5):
+    """Create a sparse version of the similarity matrix, by filtering out scores below a similarity_threshold."""
+    sparse_similarity_matrix = sparse.csr_matrix(
+        np.where(similarity_matrix >= similarity_threshold, similarity_matrix, 0)
+    )
+    return sparse_similarity_matrix
+
+
+def run_leiden_clustering(
+    similarity_matrix: np.ndarray,
     algorithm: str,
     similarity_threshold: float,
-    n_seeds: int = 2, 
-    n_iterations: int = -1, 
-    resolution_parameter: float = 1.0, 
-    **kwargs
-    ) -> np.ndarray:
-	"""Run Leiden clustering.
+    n_seeds: int = 2,
+    n_iterations: int = -1,
+    resolution_parameter: float = 1.0,
+    **kwargs,
+) -> np.ndarray:
+    """Run Leiden clustering.
 
     Types of Leiden clustering:
         (1) Standard Leiden clustering: Modularity, Resolution parameter = 1.0
@@ -93,10 +98,10 @@ def run_leiden_clustering(similarity_matrix: np.ndarray,
         resolution_parameter (float, optional)
         **kwargs: Any additional arguments to be passed to the clustering algorithm of
           choice.
-        
+
     Returns:
         A np.ndarray of memberships
-        """
+    """
     # Open kwargs
     for key, value in kwargs.items():
         if key == "n_seeds":
@@ -105,86 +110,92 @@ def run_leiden_clustering(similarity_matrix: np.ndarray,
             n_iterations = value
         if key == "resolution":
             resolution_parameter = value
-    
+
     # Convert similarity matrix into sparse csr matrix
-    sparse_similarity_matrix = create_sparse_csr(similarity_matrix, similarity_threshold)
+    sparse_similarity_matrix = create_sparse_csr(
+        similarity_matrix, similarity_threshold
+    )
 
     # Convert sparse matrix to edge list
-	n_vertices = sparse_similarity_matrix.shape[0]  # Assuming square similarity matrix
-	rows, cols = sparse_similarity_matrix.nonzero() # Construct edges from non-zero entries
-	edges = list(zip(rows, cols))
-	weights = sparse_similarity_matrix.data
+    n_vertices = sparse_similarity_matrix.shape[0]  # Assuming square similarity matrix
+    rows, cols = (
+        sparse_similarity_matrix.nonzero()
+    )  # Construct edges from non-zero entries
+    edges = list(zip(rows, cols))
+    weights = sparse_similarity_matrix.data
 
-	# Create igraph object
-	g = ig.Graph(n_vertices, edges=edges)
-	g.es['weight'] = weights # Assign edge attribute
+    # Create igraph object
+    g = ig.Graph(n_vertices, edges=edges)
+    g.es["weight"] = weights  # Assign edge attribute
 
-	best_quality = None
-	best_membership = None
+    best_quality = None
+    best_membership = None
 
-    for seed in range(1, n_seeds+1):
+    for seed in range(1, n_seeds + 1):
         if algorithm == "leiden":
             partition = leiden_clustering(g, n_iterations, seed)
         elif algorithm == "weighted_leiden":
-            partition = weighted_leiden_clustering(g, partition_type, resolution_parameter, n_iterations, seed)
+            partition = weighted_leiden_clustering(
+                g, partition_type, resolution_parameter, n_iterations, seed
+            )
         elif algorithm == "cpm_leiden":
-            partition = cpm_leiden_clustering(g, partition_type, resolution_parameter, n_iterations, seed)
+            partition = cpm_leiden_clustering(
+                g, partition_type, resolution_parameter, n_iterations, seed
+            )
         else:
             raise ValueError(f"Unsupported Leiden clustering type: {partition_type}")
 
-		quality = partition.quality()
-		membership = np.array(partition.membership)
-		
-		if best_quality is None or quality > best_quality:
-			best_quality = quality
-			best_membership = membership
+    quality = partition.quality()
+    membership = np.array(partition.membership)
 
-	return best_membership
+    if best_quality is None or quality > best_quality:
+        best_quality = quality
+        best_membership = membership
+    return best_membership
 
-def leiden_clustering(graph: ig.Graph, 
-    n_iterations: int, 
-    seed: int
-    ):
+
+def leiden_clustering(graph: ig.Graph, n_iterations: int, seed: int):
     """Standard Leiden clustering: Modularity, Resolution parameter = 1.0"""
     partition_type == la.ModularityVertexPartition
     partition = la.find_partition(
-            graph=g,
-            partition_type=partition_type,
-            weights='weight', # Use edge attribute of graph
-            n_iterations=n_iterations,
-            seed=seed*100)
+        graph=g,
+        partition_type=partition_type,
+        weights="weight",  # Use edge attribute of graph
+        n_iterations=n_iterations,
+        seed=seed * 100,
+    )
     return partition
 
-def weighted_leiden_clustering(g: ig.Graph, 
-    resolution_parameter: float, 
-    n_iterations: int,
-    seed: int
-    ):
+
+def weighted_leiden_clustering(
+    g: ig.Graph, resolution_parameter: float, n_iterations: int, seed: int
+):
     """Weighted Leiden clustering: Modularity, Resolution parameter enabled"""
     partition_type == la.RBConfigurationVertexPartition
     partition = la.find_partition(
         graph=g,
         partition_type=partition_type,
-        weights='weight', # Use edge attribute of graph
+        weights="weight",  # Use edge attribute of graph
         resolution_parameter=resolution_parameter,
         n_iterations=n_iterations,
-        seed=seed*100)
+        seed=seed * 100,
+    )
     return partition.membership
 
-def cpm_leiden_clustering(g: ig.Graph, 
-    resolution_parameter: float, 
-    n_iterations: int,
-    seed: int
-    ):
+
+def cpm_leiden_clustering(
+    g: ig.Graph, resolution_parameter: float, n_iterations: int, seed: int
+):
     """Constant Potts Model (CPM) Leiden clustering: CPM Qualty, Resolution parameter enabled"""
     partition_type == la.CPMVertexPartition
     partition = la.find_partition(
         graph=g,
         partition_type=partition_type,
-        weights='weight', # Use edge attribute of graph
+        weights="weight",  # Use edge attribute of graph
         resolution_parameter=resolution_parameter,
         n_iterations=n_iterations,
-        seed=seed*100)
+        seed=seed * 100,
+    )
     return partition.membership
 
 

MotifCompendium/utils/motif.py
@@ -77,78 +77,90 @@ def motif_to_df(motif: np.ndarray) -> pd.DataFrame:
     """Transforms a motif into a pd.DataFrame ready for plotting with logomaker."""
     return pd.DataFrame(motif, columns=["A", "C", "G", "T"])
 
+
 def cwm4_to_cwm8(cwm4: np.array) -> np.array:
-	"""Expand base pair to A+,C+,G+,T+,A-,C-,G-,T-"""
-	rows, cols = cwm4.shape
-	cwm8 = np.zeros((rows, 2*cols))
+    """Expand base pair to A+,C+,G+,T+,A-,C-,G-,T-"""
+    rows, cols = cwm4.shape
+    cwm8 = np.zeros((rows, 2 * cols))
 
-	cwm8[:,:cols] = np.where(cwm4 > 0, cwm4, 0)
-	cwm8[:,cols:] = np.where(cwm4 < 0, -cwm4, 0)
+    cwm8[:, :cols] = np.where(cwm4 > 0, cwm4, 0)
+    cwm8[:, cols:] = np.where(cwm4 < 0, -cwm4, 0)
 
-	return cwm8
+    return cwm8
+
 
 def cwm8_to_pair28(cwm8: np.array) -> np.array:
-	"""Expand A+,C+,G+,T+,A-,C-,G-,T- to non-repeating, without order dinucleotide pair combinations"""
-	# Current dimensions: cwm8
-	rows, cols = cwm8.shape
-	nuc = 2 # Dinucleotide pair
-	
-	# New dimensions
-	new_cols = int(np.math.factorial(cols) / (np.math.factorial(cols-nuc) * np.math.factorial(nuc))) # C(n,r)
-	new_cols_2 = int(new_cols/2)
-	pair28 = np.zeros((rows, new_cols))
+    """Expand A+,C+,G+,T+,A-,C-,G-,T- to non-repeating, without order dinucleotide pair combinations"""
+    # Current dimensions: cwm8
+    rows, cols = cwm8.shape
+    nuc = 2  # Dinucleotide pair
 
-	## Matrix multiplication + mask
-	mask = np.tril(np.ones(cols), k=-1).astype(bool) # Mask for bottom left off-diagonal half
-	
-	for i in range(rows):
-		pair_perm = np.outer(cwm8[i,:], cwm8[i,:]) # Permutations: With order
-		pair28[i,:] = pair_perm[mask] # Combinations: Without order; Exclude self (e.g., AA,CC,GG,TT)
+    # New dimensions
+    new_cols = int(
+        np.math.factorial(cols)
+        / (np.math.factorial(cols - nuc) * np.math.factorial(nuc))
+    )  # C(n,r)
+    new_cols_2 = int(new_cols / 2)
+    pair28 = np.zeros((rows, new_cols))
 
-	return pair28
+    ## Matrix multiplication + mask
+    mask = np.tril(np.ones(cols), k=-1).astype(
+        bool
+    )  # Mask for bottom left off-diagonal half
+
+    for i in range(rows):
+        pair_perm = np.outer(cwm8[i, :], cwm8[i, :])  # Permutations: With order
+        pair28[i, :] = pair_perm[
+            mask
+        ]  # Combinations: Without order; Exclude self (e.g., AA,CC,GG,TT)
+
+    return pair28
+
 
 def cwm8_to_dinuc64(cwm8: np.array) -> np.array:
-	"""Evaluate two positions at a time, as non-self A+,C+,G+,T+,A-,C-,G-,T- dinucleotide pair permutations"""
-	# Current dimensions: cwm8
-	rows, cols = cwm8.shape
+    """Evaluate two positions at a time, as non-self A+,C+,G+,T+,A-,C-,G-,T- dinucleotide pair permutations"""
+    # Current dimensions: cwm8
+    rows, cols = cwm8.shape
 
-	# Dimensions: Dinuc64
-	new_rows = rows // 2 # Drop final position if odd length
-	new_cols = cols ** 2 # Include self-repeats
-	dinuc64 = np.zeros((new_rows, new_cols))
+    # Dimensions: Dinuc64
+    new_rows = rows // 2  # Drop final position if odd length
+    new_cols = cols**2  # Include self-repeats
+    dinuc64 = np.zeros((new_rows, new_cols))
 
-	# Calculate dinucleotide pairs, as product of distributions	
-	# Calculate dinucleotide pairs, as product of distributions	
-	mask = ~np.eye(cols, dtype=bool)
+    # Calculate dinucleotide pairs, as product of distributions
+    # Calculate dinucleotide pairs, as product of distributions
+    mask = ~np.eye(cols, dtype=bool)
 
-	# Calculate dinucleotide pairs, as product of distributions
-	mask = ~np.eye(cols, dtype=bool)
+    # Calculate dinucleotide pairs, as product of distributions
+    mask = ~np.eye(cols, dtype=bool)
 
-	for i in range(new_rows):
-		dinuc_pair = np.outer(cwm8[2*i,:], cwm8[2*i+1,:])
-		dinuc64[i,:] = dinuc_pair.flatten()
+    for i in range(new_rows):
+        dinuc_pair = np.outer(cwm8[2 * i, :], cwm8[2 * i + 1, :])
+        dinuc64[i, :] = dinuc_pair.flatten()
 
-	return dinuc64
+    return dinuc64
+
 
 def shannon_entropy(prob_array: np.array, epsilon: float = 1e-10) -> float:
     # Normalize, flatten array
-	prob_array = prob_array / np.sum(prob_array)
-	prob_array = prob_array.flatten()
+    prob_array = prob_array / np.sum(prob_array)
+    prob_array = prob_array.flatten()
 
-	# Replace zeroes with epsilon
-	prob_array[prob_array == 0] = epsilon
-	length = prob_array.shape[0]
+    # Replace zeroes with epsilon
+    prob_array[prob_array == 0] = epsilon
+    length = prob_array.shape[0]
 
-	# Calculate Shannon entropy
-	entropy = -np.sum(prob_array * np.log2(prob_array)) / np.log2(length)
+    # Calculate Shannon entropy
+    entropy = -np.sum(prob_array * np.log2(prob_array)) / np.log2(length)
 
-	return entropy
+    return entropy
+
 
 def calculate_entropy(cwm4: np.array, length: int = 30) -> tuple:
     """Calculate entropy metrics, to quantify motif information complexity.
 
     List of Entropy metrics:
-        (1) Base entropy: 
+        (1) Base entropy:
             Calculation: Shannon entropy on (30,8)
             Purpose:    (High) Archetype #1: Noise/chaos
                         (Low) Archetype #2: Sharp nucleotide peak (e.g., G)
@@ -172,55 +184,58 @@ def calculate_entropy(cwm4: np.array, length: int = 30) -> tuple:
           And if all bases are present and represented equally, the weights will for all
           bases at that position will be set to 0.
     """
-	if not isinstance(cwm4, np.ndarray):
-		raise TypeError("cwm4 must be a NumPy array")
-	if len(cwm4.shape) != 2:
-		raise ValueError("cwm4 must be a 2D array")
-	
-	# Find normalized, standard length CWM
-	cwm4_prob = cwm4 / np.sum(cwm4)
-	rows, cols = cwm4.shape
+    if not isinstance(cwm4, np.ndarray):
+        raise TypeError("cwm4 must be a NumPy array")
+    if len(cwm4.shape) != 2:
+        raise ValueError("cwm4 must be a 2D array")
 
-	# Create cwm8, pair28, dinuc64, as probability
-	cwm8 = cwm4_to_cwm8(cwm4)
-	cwm8_prob = cwm8 / np.sum(cwm8)
+    # Find normalized, standard length CWM
+    cwm4_prob = cwm4 / np.sum(cwm4)
+    rows, cols = cwm4.shape
 
-	pair28 = cwm8_to_pair28(cwm8)
-	pair28_prob = pair28 / np.sum(pair28)
+    # Create cwm8, pair28, dinuc64, as probability
+    cwm8 = cwm4_to_cwm8(cwm4)
+    cwm8_prob = cwm8 / np.sum(cwm8)
 
-	dinuc64 = cwm8_to_dinuc64(cwm8)
-	dinuc64_prob = dinuc64 / np.sum(dinuc64)
+    pair28 = cwm8_to_pair28(cwm8)
+    pair28_prob = pair28 / np.sum(pair28)
 
-	# Sum across bases (w,), normalize
-	pos_prob = np.sum(cwm8_prob, axis=1) / np.sum(cwm8_prob)
-	pair_pos_prob = np.sum(pair28_prob, axis=1) / np.sum(pair28_prob)	
-	dinuc_pos_prob = np.sum(dinuc64_prob, axis=1) / np.sum(dinuc64_prob)
-	
-	# Sum across positions (8 or 64,), normalize
-	base_prob = np.sum(cwm8_prob, axis=0) / np.sum(cwm8_prob)
-	pair_base_prob = np.sum(pair28_prob, axis=0) / np.sum(pair28_prob)	
-	dinuc_base_prob = np.sum(dinuc64_prob, axis=0) / np.sum(dinuc64_prob)
+    dinuc64 = cwm8_to_dinuc64(cwm8)
+    dinuc64_prob = dinuc64 / np.sum(dinuc64)
 
-	# Calulcate entropy metrics
-	# (1) Base entropy
-	cwm_entropy = shannon_entropy(cwm8_prob)
+    # Sum across bases (w,), normalize
+    pos_prob = np.sum(cwm8_prob, axis=1) / np.sum(cwm8_prob)
+    pair_pos_prob = np.sum(pair28_prob, axis=1) / np.sum(pair28_prob)
+    dinuc_pos_prob = np.sum(dinuc64_prob, axis=1) / np.sum(dinuc64_prob)
 
-	# (2) Pos-base Entropy ratio:
-	pos_entropy = shannon_entropy(pos_prob)
-	base_entropy = shannon_entropy(base_prob)
-	entropy_ratio = pos_entropy / base_entropy # High entropy when: High positional = Broad profile, Low base: Single base
+    # Sum across positions (8 or 64,), normalize
+    base_prob = np.sum(cwm8_prob, axis=0) / np.sum(cwm8_prob)
+    pair_base_prob = np.sum(pair28_prob, axis=0) / np.sum(pair28_prob)
+    dinuc_base_prob = np.sum(dinuc64_prob, axis=0) / np.sum(dinuc64_prob)
 
-	# (3) Pair nucleotide ratio:
-	pair_pos_entropy = shannon_entropy(pair_pos_prob)
-	pair_base_entropy = shannon_entropy(pair_base_prob)
-	pair_entropy_ratio = pair_pos_entropy / pair_base_entropy
+    # Calulcate entropy metrics
+    # (1) Base entropy
+    cwm_entropy = shannon_entropy(cwm8_prob)
 
-	# (4) Dinucleotide ratio
-	dinuc_pos_entropy = shannon_entropy(dinuc_pos_prob)
-	dinuc_base_entropy = shannon_entropy(dinuc_base_prob)
-	dinuc_entropy_ratio = dinuc_pos_entropy / dinuc_base_entropy
+    # (2) Pos-base Entropy ratio:
+    pos_entropy = shannon_entropy(pos_prob)
+    base_entropy = shannon_entropy(base_prob)
+    entropy_ratio = (
+        pos_entropy / base_entropy
+    )  # High entropy when: High positional = Broad profile, Low base: Single base
 
-	return (cwm_entropy, entropy_ratio, pair_entropy_ratio, dinuc_entropy_ratio)
+    # (3) Pair nucleotide ratio:
+    pair_pos_entropy = shannon_entropy(pair_pos_prob)
+    pair_base_entropy = shannon_entropy(pair_base_prob)
+    pair_entropy_ratio = pair_pos_entropy / pair_base_entropy
+
+    # (4) Dinucleotide ratio
+    dinuc_pos_entropy = shannon_entropy(dinuc_pos_prob)
+    dinuc_base_entropy = shannon_entropy(dinuc_base_prob)
+    dinuc_entropy_ratio = dinuc_pos_entropy / dinuc_base_entropy
+
+    return (cwm_entropy, entropy_ratio, pair_entropy_ratio, dinuc_entropy_ratio)
+
 
 #####################
 # PRIVATE CONSTANTS #
@@ -238,11 +253,14 @@ _MOTIF_4_TO_8_NEG[2, 4] = 1
 _MOTIF_4_TO_8_NEG[3, 6] = 1
 
 # Suggested entropy metric thresholds for chrombpnet-based TF-Modisco motifs
-ENTROPY_THRESHOLD_DICT = {'1_singlepeak': ('cwm_entropy', 'lt', 0.4),
-                     '2_noisemix': ('cwm_entropy', 'gt', 0.75),
-                     '3_broadsingle': ('entropy_ratio', 'gt', 3.0),
-                     '4_broadbias': ('pair_entropy_ratio', 'gt', 3.0),
-                     '5_broadCpG': ('dinuc_entropy_ratio', 'gt', 4.0)}
+ENTROPY_THRESHOLD_DICT = {
+    "1_singlepeak": ("cwm_entropy", "lt", 0.4),
+    "2_noisemix": ("cwm_entropy", "gt", 0.75),
+    "3_broadsingle": ("entropy_ratio", "gt", 3.0),
+    "4_broadbias": ("pair_entropy_ratio", "gt", 3.0),
+    "5_broadCpG": ("dinuc_entropy_ratio", "gt", 4.0),
+}
+
 
 #####################
 # PRIVATE FUNCTIONS #