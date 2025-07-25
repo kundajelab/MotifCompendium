67f442cf95d57df6b205c11c6c0631f6d04342ed
Salil Deshpande <salil512@stanford.edu>
2025-07-25 14:34:56 +0000

    v1.0.0 commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="MotifCompendium",
-    version="1.0",
+    version="1.0.0",
     packages=find_packages(),
     package_data={
         "MotifCompendium": ["utils/*"],
