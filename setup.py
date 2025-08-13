f21248f96a5f8c57490339e17683945b0ad27933
Chang M. Yun <chang.m.yun@stanford.edu>
2025-08-13 22:48:17 +0000

    Commit v1.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="MotifCompendium",
-    version="1.0.3",
+    version="1.0.5",
     packages=find_packages(),
     package_data={
         "MotifCompendium": ["utils/*"],
