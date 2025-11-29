fix numerical bug with cpu similarity (+ minor changes to gpu/overall)

src/MotifCompendium/utils/similarity.py
@@ -49,21 +49,18 @@ def compute_similarities(
         if L != L0:
             raise ValueError("All motifs must have the same length.")
     # PREPROCESS
-    if unsigned:
-        motif_stack_list = [
-            np.abs(motif_stack) for motif_stack in motif_stack_list
-        ]  # Unsigned
     if utils_config.get_ic_scaled_similarity():
         motif_stack_list = [
             utils_motif.ic_scale(motif_stack) for motif_stack in motif_stack_list
         ]  # IC scale
-    motif_stack_list = [
-        utils_motif.motif_4_to_8(motif_stack) for motif_stack in motif_stack_list
-    ]  # 8 channel
-    motif_stack_list = [
-        motif_stack / np.linalg.norm(motif_stack, axis=(1, 2), keepdims=True)
-        for motif_stack in motif_stack_list
-    ]  # L2 normalize
+    if unsigned:
+        motif_stack_list = [
+            np.abs(motif_stack) for motif_stack in motif_stack_list
+        ]  # Unsigned
+    else:
+        motif_stack_list = [
+            utils_motif.motif_4_to_8(motif_stack) for motif_stack in motif_stack_list
+        ]  # 8 channel
     # COMPUTE
     if utils_config.get_max_chunk() != -1:
         (

src/MotifCompendium/utils/similarity_core_cpu.py
@@ -72,7 +72,7 @@ def _normalize_mtx(X):
 
 
 @njit(cache=True)
-def _compute_similarity(motif_set_1, motif_set_2, xp):
+def _compute_similarity(motif_set_1, motif_set_2):
     """Computes similarity and alignment for two sets of motifs."""
     # Get shapes
     N, L, K = motif_set_1.shape
@@ -158,7 +158,7 @@ def _tensor3_matmul_tensor2(X, Y):
 @njit
 def _max_and_shifted_argmax_along_axis2(X, shift=0):
     N, M, L = X.shape
-    max_vals = np.empty((N, M), dtype=np.single)
+    max_vals = np.empty((N, M), dtype=np.float64)
     max_idxs = np.empty((N, M), dtype=np.short)
     for i in range(N):
         for j in range(M):